9eb1ccc69ebc8fcc2b1d481ab3ae09e2053e7d9b
JenChieh <jcs090218@gmail.com>
2025-01-01 01:26:36 -0800

    Update copyright year

--- eglot-shader.el
+++ eglot-shader.el
@@ -1,6 +1,6 @@
 ;;; eglot-shader.el --- Eglot Clients for Shader  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
